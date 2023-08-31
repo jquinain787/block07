5284e8fc293aa5cb4ec68783c03c685e4d7d7c5c
jquinain787 <141972848+jquinain787@users.noreply.github.com>
2023-08-31 18:58:27 -0500

    Add files via upload

---
+++ securevault.js
@@ -0,0 +1,17 @@
+/* Create a variable named message that contains the message. Create three new variables that contain operators that equal to the password 10 - 40 - 39
+then output the meaage as well as the password together */
+
+// Here we created a variable containing a string with message
+const message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+
+// Here we came up with the password using operators
+const num_1 = 5 + 5;
+
+const num_2 = 10 * 4;
+
+const num_3 = 40 - 1;
+
+
+//We then output the message with the password
+alert(`${message} ${num_1} - ${num_2} - ${num_3}`)
